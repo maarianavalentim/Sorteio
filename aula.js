223d590c445bcb6a003b697504a09082e657a0f6
maarianavalentim <valentimmariana26@gmail.com>
2023-08-04 15:15:50 -0300

    Initial commit

---
+++ aula.js
@@ -0,0 +1,16 @@
+//math.random()gera numeros aleatorios
+console.log(Math.random())
+console.log(Math.random()*20)
+console.log(Math.random(7 -3 + 1)+3)
+
+//arredonda para o valor inteiro mais proximo
+console.log(Math.round (3.1))
+console.log(Math.round (3.9))
+
+//arrendonda para baixo
+console.log(Math.floor(3.1))
+console.log(Math.floor(3.9))
+
+//arrendonda para cima
+console.log(Math.ceil(3.1))
+console.log(Math.ceil(3.9))
